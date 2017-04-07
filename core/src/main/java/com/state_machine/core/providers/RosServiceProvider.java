a8c23a2e19bd6a274103f855f4903c19bb4128da
Gao Changyu <gaochangyu@163.com>
2017-04-07 10:09:09 +0800

    remove hover_controller_msgs from the project.

--- core/src/main/java/com/state_machine/core/providers/RosServiceProvider.java
+++ core/src/main/java/com/state_machine/core/providers/RosServiceProvider.java
@@ -1,7 +1,6 @@
 package com.state_machine.core.providers;
 
 import mavros_msgs.*;
-import hover_controller_msgs.*;
 import org.apache.commons.logging.Log;
 import org.ros.exception.ServiceNotFoundException;
 import org.ros.node.ConnectedNode;
@@ -15,8 +14,6 @@ public class RosServiceProvider {
     private ServiceClient<CommandTOLRequest, CommandTOLResponse> landService;
     private ServiceClient<SetModeRequest, SetModeResponse> setFCUModeService;
     private ServiceClient<ParamPullRequest, ParamPullResponse> paramPullService;
-    private ServiceClient<SwitchModeRequest,SwitchModeResponse> setHoverControllerModeService;
-    private ServiceClient<WayPointRequest, WayPointResponse> setHoverControllerWayPointService;
     private Log log;
 
     public RosServiceProvider(ConnectedNode node) throws ServiceNotFoundException {
@@ -25,13 +22,6 @@ public class RosServiceProvider {
         armingService = node.newServiceClient("mavros/cmd/arming", CommandBool._TYPE);
         takeoffService = node.newServiceClient("mavros/cmd/takeoff",CommandTOL._TYPE);
         landService = node.newServiceClient("mavros/cmd/land",CommandTOL._TYPE);
-        try {
-            setHoverControllerModeService = node.newServiceClient("hovercontroller/switchmode", SwitchMode._TYPE);
-            setHoverControllerWayPointService = node.newServiceClient("hovercontroller/setnewwaypoint", hover_controller_msgs.WayPoint._TYPE);
-        }
-        catch(Exception e){
-            log.debug("hover controller related services not exist.");
-        }
         setFCUModeService = node.newServiceClient("mavros/set_mode", SetMode._TYPE);
         paramPullService = node.newServiceClient("mavros/param/pull",ParamPull._TYPE);
     }
@@ -48,18 +38,12 @@ public class RosServiceProvider {
         return setFCUModeService;
     }
 
-    public ServiceClient<SwitchModeRequest, SwitchModeResponse> getSetHoverControllerModeService(){ return setHoverControllerModeService; };
-
-    public ServiceClient<WayPointRequest, WayPointResponse> getSetHoverControllerWayPointService() { return setHoverControllerWayPointService;};
-
     public ServiceClient<ParamPullRequest, ParamPullResponse> getParamPullService() {return paramPullService;}
 
 
 
     public boolean isConnected(){
         return armingService.isConnected()
-                //&& setHoverControllerModeService.isConnected()
-                //&& setHoverControllerWayPointService.isConnected()
                 && setFCUModeService.isConnected()
                 && paramPullService.isConnected()
                 && takeoffService.isConnected()
