eb5dbe45c080349f1d8b0f2634f459e1efb062ff
Gao Changyu <gaochangyu@163.com>
2017-04-15 12:38:39 +0800

    add apache v2.0 license information

--- core/src/main/java/com/navigator/core/actions/ExternalAction.java
+++ core/src/main/java/com/navigator/core/actions/ExternalAction.java
@@ -20,9 +20,22 @@ import org.ros.node.service.ServiceResponseListener;
 import org.ros.node.topic.Publisher;
 import org.ros.node.topic.Subscriber;
 
-/**
- * Created by edward on 17-3-26.
+/*
+ * Copyright (C) 2016-2017 Gao Changyu.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
  */
+
 public class ExternalAction extends Action {
 
     private Log log;
--- core/src/main/java/com/navigator/core/stateMachine/utils/StateControl.java
+++ core/src/main/java/com/navigator/core/stateMachine/utils/StateControl.java
@@ -12,9 +12,22 @@ import org.ros.node.service.ServiceResponseBuilder;
 
 import java.util.Queue;
 
-/**
- * Created by firefly on 12/9/16.
+/*
+ * Copyright (C) 2016-2017 Gao Changyu.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
  */
+
 public class StateControl {
     private Queue<State> stateQueue;
     private FileProvider fileProvider;
